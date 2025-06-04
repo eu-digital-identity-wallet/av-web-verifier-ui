6e22d403ccde484f780cea16fd3cfc8b43c2c793
Manolis Viennas <viennas@gmail.com>
2025-06-04 14:54:17 +0300

    av-profile

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -4,19 +4,49 @@
 
 import { QRCodeSVG } from 'qrcode.react';
 
-export default function QrCode({
-  data,
-}: {
-  data: { client_id: string; request_uri: string; request_uri_method: string };
-}) {
+export default function QrCode(data: any) {
   return (
-    <QRCodeSVG
-      value={`eudi-openid4vp://?client_id=${encodeURIComponent(
-        data.client_id
-      )}&request_uri=${encodeURIComponent(
-        data.request_uri
-      )}&request_uri_method=${encodeURIComponent(data.request_uri_method)}`}
-      className=" rounded h-full max-h-96 w-auto"
-    />
+    <>
+     <QRCodeSVG value={parseJwtAndCreateUri(data)}
+      className="rounded h-full max-h-96 w-auto"
+      />
+      <a href={parseJwtAndCreateUri(data)} className='mt-4 text-center' style={{ textDecoration: 'underline', color:'blue' }}>For mobile login click here</a>
+    </>
+   
   );
+}
+
+function parseJwtAndCreateUri(token:any) {
+
+    if (!token) {
+        throw new Error("Token is undefined or empty");
+    }
+    var parts = token.data.split('.');
+    if (parts.length !== 3) {
+        throw new Error("Token does not have the expected 3 parts");
+    }
+    var base64Url = parts[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(
+        atob(base64)
+            .split('')
+            .map(function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join('')
+    );
+    var parsed = JSON.parse(jsonPayload);
+
+    var request_new = "avsp"
+    + "://" 
+    + "?client_id=redirect_uri:" + parsed.response_uri
+    + "&response_type=" + parsed.response_type
+    + "&response_mode=" + parsed.response_mode
+    + "&response_uri=" + parsed.response_uri
+    + "&presentation_definition_uri=" + parsed.presentation_definition_uri
+    + "&dcql=null"
+    + "&nonce=" + parsed.nonce
+    + "&state=" + parsed.state
+
+    return request_new;
 }
